5cecf80ba703e47df494d2659b21477c8f83761a
santhosh <santhosh44.sk44@gmail.com>
2021-09-21 17:03:22 +0530

    first commit

---
+++ mobile-microservice-application/src/main/java/com/santhosh/services/MobileMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.santhosh.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileMicroserviceApplication.class, args);
+	}
+
+}
---
+++ mobile-microservice-application/src/test/java/com/santhosh/services/MobileMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhosh.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
