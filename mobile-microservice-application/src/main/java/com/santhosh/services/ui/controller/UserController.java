155799765685ec55225f1ea4d1aaf38278400551
santhosh <santhosh22.vtalent@gmail.com>
2021-09-22 11:17:27 +0530

    mediatype added for getusers method

--- mobile-microservice-application/src/main/java/com/santhosh/services/ui/controller/UserController.java
+++ mobile-microservice-application/src/main/java/com/santhosh/services/ui/controller/UserController.java
@@ -1,19 +1,38 @@
 package com.santhosh.services.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.santhosh.services.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+	
 	@GetMapping
-	public String getUser() {
-		return "get user method was called";
+	public String getUsers(@RequestParam(value="page") int page,@RequestParam(value = "limit") int limit)
+	{
+		return "get users method was called with page =" + page + "and limit = "+ limit;
+	}
+	@GetMapping(path = "/{userId}",produces= 
+		{
+			MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE
+		})
+	public UserRest getUser(@PathVariable String userId) {
+		
+		UserRest response = new UserRest();
+		response.setFirstName("santhosh");
+		response.setLastName("kumar");
+		response.setEmail("san@gmail.com");
+		return response;
 	}
 
 	@PostMapping
