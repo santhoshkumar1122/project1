70cfdd0ca22b71a95bf324e7ee534afcda48c25a
santhoshkumar1122 <46085729+santhoshkumar1122@users.noreply.github.com>
2021-09-22 16:59:52 +0530

    post method changed

--- mobile-microservice-application/src/main/java/com/santhosh/services/ui/controller/UserController.java
+++ mobile-microservice-application/src/main/java/com/santhosh/services/ui/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.santhosh.services.ui.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.santhosh.services.ui.model.response.UserRest;
+import com.santhosh.services.ui.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -26,18 +30,32 @@ public class UserController {
 			MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_JSON_VALUE
 		})
-	public UserRest getUser(@PathVariable String userId) {
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		
 		UserRest response = new UserRest();
 		response.setFirstName("santhosh");
 		response.setLastName("kumar");
 		response.setEmail("san@gmail.com");
-		return response;
+		return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
 	}
 
 	@PostMapping
-	public String createUser() {
-		return "create user method was called";
+	(consumes = {
+			MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE
+	},
+	produces = {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE
+	}
+			)
+	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRest response =new UserRest();
+		response.setEmail(userDetails.getEmail());
+		response.setFirstName(userDetails.getFirstName());
+		response.setLastName(userDetails.getLastName());;
+		
+	return new ResponseEntity<UserRest>(response,HttpStatus.OK);
 
 	}
 
